cba3a175d1f39d39dfb74b8ef6c8fcfcfe421e4b
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-17 14:13:32 +0200

    Add header and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EventEditComponent } from './event-edit/event-edit.component';
+import { HeaderComponent } from './header/header.component';
+
 
 const routes: Routes = [
-  { path: 'event/:id/edit', component: EventEditComponent }
+  { path: 'event/:id/edit', component: EventEditComponent },
+  { path: '', component: HomeComponent }, // Home route
+  { path: 'event/:id/edit', component: EventEditComponent },
+  { path: 'categories', component: CategoriesComponent },
 ];
 
 @NgModule({
