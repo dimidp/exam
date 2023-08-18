8f380bb5a50a28e027564eee79ec56ff834907c0
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-18 14:07:16 +0200

    add category filter

--- src/app/list-display/list-display.component.ts
+++ src/app/list-display/list-display.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Event } from '../event.interface';
+import { Category } from '../category.interface';
 import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { PageEvent } from '@angular/material/paginator';
 import { map } from 'rxjs/operators';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-list-display',
@@ -19,12 +21,15 @@ export class ListDisplayComponent implements OnInit {
   currentPage: number = 0;
   pageSize: number = 10;
 
-  searchQuery: string = ''; // Hinzugefügt: Das ist die Suchanfrage
+  searchQuery: string = '';
+  selectedCategory: number | undefined;
+  allCategories: Category[] = [];
 
-  constructor(private router: Router, private dataService: DataService) {}
+  constructor(private router: Router, private dataService: DataService, private categoryService: CategoryService) {}
 
   ngOnInit() {
     this.loadEvents();
+    this.loadCategories();
 
     this.dataService.eventsChanged.subscribe(() => {
       this.loadEvents();
@@ -35,7 +40,13 @@ export class ListDisplayComponent implements OnInit {
     this.dataService.getAllEvents().subscribe(events => {
       this.totalEvents = events.length;
       this.events$ = of(events);
-      this.updatePagedEvents(events); // Änderung: aktualisierte Methode aufrufen
+      this.updatePagedEvents(events);
+    });
+  }
+
+  loadCategories() {
+    this.categoryService.getAllCategories().subscribe(categories => {
+      this.allCategories = categories;
     });
   }
 
@@ -44,27 +55,29 @@ export class ListDisplayComponent implements OnInit {
     this.updatePagedEvents();
   }
 
+  onSearch() { // Hier ist die korrekte Methode
+    this.updatePagedEvents();
+  }
+
+  onCategoryChange() {
+    this.updatePagedEvents();
+  }
+
   updatePagedEvents(events?: Event[]) {
     if (!events) {
       this.pagedEvents$ = this.events$.pipe(
-        map(events => this.applySearchFilter(events))
+        map(events => this.applyFilters(events))
       );
     } else {
-      this.pagedEvents$ = of(this.applySearchFilter(events));
+      this.pagedEvents$ = of(this.applyFilters(events));
     }
   }
 
-  applySearchFilter(events: Event[]): Event[] {
-    return events.filter(event =>
-      event.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-    ).slice(
-      this.currentPage * this.pageSize,
-      (this.currentPage + 1) * this.pageSize
-    );
-  }
-
-  onSearch() {
-    this.updatePagedEvents();
+  applyFilters(events: Event[]): Event[] {
+    return events
+      .filter(event => event.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      .filter(event => !this.selectedCategory || event.categories.some(cat => cat.id === this.selectedCategory))
+      .slice(this.currentPage * this.pageSize, (this.currentPage + 1) * this.pageSize);
   }
 
   editEvent(eventId: number) {
