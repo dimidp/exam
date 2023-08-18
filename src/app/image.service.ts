9cc61e6dc26ab2bbce0ee8c2539d3bd3d6c3974f
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-18 10:32:43 +0200

    Implement image service

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private config: ConfigService) { }
+
+  addOrUpdateImageToEvent(eventId: string, imageData: string) {
+    const url = `${this.config.baseURL}/${this.config.user}/images/${eventId}`;
+    const imagePayload = {
+      imagedata: imageData
+    };
+    return this.http.post(url, imagePayload);
+  }
+
+  removeImageFromEvent(eventId: string) {
+    const url = `${this.config.baseURL}/${this.config.user}/images/${eventId}`;
+    return this.http.delete(url);
+  }
 }
