b8f8b071ed66a831aba77ae7a14af1a40322ec9c
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-18 11:49:38 +0200

    Add image functionality

--- src/app/event-detail-view/event-detail-view.component.ts
+++ src/app/event-detail-view/event-detail-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
+import { ImageService } from '../image.service';
 import { Event } from '../event.interface';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
@@ -12,8 +13,14 @@ import { Router } from '@angular/router';
 })
 export class EventDetailViewComponent implements OnInit {
   event$: Observable<Event> | undefined;
+  selectedImageFile: File | undefined;
 
-  constructor(private router:Router, private route: ActivatedRoute, private dataService: DataService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    private imageService: ImageService
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -21,11 +28,30 @@ export class EventDetailViewComponent implements OnInit {
       this.loadEvent(eventId);
     });
   }
+
   goBack() {
     this.router.navigate(['/events']); 
   }
 
-  loadEvent(eventId: number) {
+  onImageFileSelected(event: any) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.selectedImageFile = event.target.files[0];
+    }
+  }
+
+  async loadEvent(eventId: number) {
     this.event$ = this.dataService.getEventByID(eventId);
+  }
+
+  async uploadImage() {
+    if (this.selectedImageFile) {
+      const imageBase64 = await this.imageService.imageToBase64(this.selectedImageFile);
+      this.route.paramMap.subscribe(params => {
+        const eventId = Number(params.get('id'));
+        this.imageService.addOrUpdateImageToEvent(eventId.toString(), imageBase64).subscribe(() => {
+          this.loadEvent(eventId);
+        });
+      });
+    }
   }
 }
